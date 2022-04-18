736628667e99a50a8bd8e9eb4bfe75071d821353
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-04-18 14:31:57 -0300

    TestActivityNotification

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewPrep
 {
@@ -7,7 +8,88 @@ namespace InterviewPrep
     {
         static void Main(string[] args)
         {
-            TestCheckMagazine();
+            TestActivityNotifications();
+        }
+        public static int activityNotifications(List<int> expenditure, int d)
+        {
+            if (expenditure.Count <= d)
+                return 0;
+
+            // Create map of frequency - for the first 'd' elements in expenditure
+            var map = new int[expenditure.Max()+1];
+            for (int i = 0; i < d; i++)
+                map[expenditure[i]]++;
+
+            // notifications count
+            int notifications = 0;
+
+            // traverse expenditure from d
+            for ( int i=d; i< expenditure.Count; i++)
+            {
+                // Get mediam from map
+                decimal median = GetMedianFromMap(map, d);
+                if (expenditure[i] >= median * 2)
+                    notifications++;
+
+                map[expenditure[i - d]]--;
+                map[expenditure[i]]++;
+            }
+
+            // return notifications count
+            return notifications;
+
+        }
+        public static decimal GetMedianFromMap(int[] map, int d) 
+        {
+            // counter
+            int c = 0;
+
+            // Even number of elements
+            if (d % 2 == 0)
+            {
+                foreach (var i in map)
+                {
+                    c += map[i];
+                    if (c == d / 2)
+                        if ( i == 1)
+                            return ((decimal)map[i] + map[i + 1]) / 2;
+                        else
+                            return map[i];
+                }
+            }
+            else
+            {
+                foreach( var i in map)
+                {
+                    c += map[i];
+                    if (c >= d / 2 + 1)
+                        return map[i];
+                }
+            }
+
+            throw new Exception("Error in map");
+        }
+        public static void TestActivityNotifications()
+        {
+            var expenditures = new List<int>() { 10, 20, 30, 40, 50 };
+            var d = 3;
+
+            Console.WriteLine(activityNotifications(expenditures, d));
+            Console.WriteLine("Expected: 1");
+
+
+            expenditures = new List<int>() { 2, 3, 4, 2, 3, 6, 8, 4, 5 };
+            d = 5;
+
+            Console.WriteLine(activityNotifications(expenditures, d));
+            Console.WriteLine("Expected: 2");
+
+            expenditures = new List<int>() { 1, 2, 3, 4, 4 };
+            d = 4;
+
+            Console.WriteLine(activityNotifications(expenditures, d));
+            Console.WriteLine("Expected: 0");
+
         }
         public static void checkMagazine(List<string> magazine, List<string> note)
         {
