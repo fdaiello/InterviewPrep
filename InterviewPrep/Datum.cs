9c0444e0531c2a82ba781148e6472cf4e30cd54c
Felipe Daiello <fdaiello@misterpostman.com.br>
2023-07-16 19:39:17 -0300

    GridChallenge

---
+++ InterviewPrep/Datum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep
+{
+    public class Games
+    {
+        public string competition { get; set; }
+        public int year { get; set; }
+        public string round { get; set; }
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+        public string team1goals { get; set; }
+        public string team2goals { get; set; }
+    }
+
+    public class Root
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public List<Games> data { get; set; }
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Winner
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public int year { get; set; }
+        public string winner { get; set; }
+        public string runnerup { get; set; }
+    }
+
+    public class Root2
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public List<Winner> data { get; set; }
+    }
+}
--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Resources;
+using System.Threading.Tasks;
 
 namespace InterviewPrep
 {
@@ -8,8 +13,167 @@ namespace InterviewPrep
     {
         static void Main(string[] args)
         {
-            TestActivityNotifications();
+            TestGridChallenge();
         }
+        public static string gridChallenge(List<string> grid)
+        {
+            char[][] grid2 = new char[grid.Count()][];
+            
+            for (int row = 0; row < grid.Count; row++)
+            {
+                grid2[row] = grid[row].ToArray();
+                Array.Sort(grid2[row]);
+            }
+
+            for ( int col=0; col < grid2[0].Count(); col++)
+            {
+                for ( int row=0; row < grid2.Count()-1; row++)
+                {
+                    if (grid2[row][col] > grid2[row+1][col])
+                        return "NO";
+                }
+            }
+
+            return "YES";
+        }
+        public static void TestGridChallenge()
+        {
+
+            var list = new List<string>() { "ebacd", "fghij", "olmkn", "trpqs", "xywuv" };
+            Console.WriteLine(gridChallenge(list));
+
+        }
+        public static int getWinnerTotalGoals(string competition, int year)
+        {
+            var winner = GetWinner(competition, year);
+            var goals = GetTotalGoalsInCompetition(competition, winner, year);
+
+            return goals;
+        }
+        public static int GetTotalGoalsInCompetition(string competition, string team, int year)
+        {
+            var lGames = GetGoals(team, year);
+            var g = 0;
+
+            foreach (var item in lGames.Where(p=>p.competition==competition))
+            {
+                if (item.team1 == team)
+                    g += Int32.Parse(item.team1goals);
+                else
+                    g += Int32.Parse(item.team2goals);
+            }
+
+            return g;
+        }
+        public static int GetTotalGoals(string team, int year)
+        {
+            var lGames = GetGoals(team, year);
+            var g = 0;
+
+            foreach (var item in lGames)
+            {
+                if (item.team1.ToLower() == team.ToLower()) 
+                    g += Int32.Parse(item.team1goals);
+                else
+                    g += Int32.Parse(item.team2goals);
+            }
+
+            return g;
+        }
+        public static void TestGetGoals()
+        {
+            var g = GetTotalGoals("Barcelona", 2011);
+            Console.WriteLine(g);
+        }
+        public static List<Games> GetGoals(string team, int year)
+        {
+            int page = 1;
+            var lGames = new List<Games>();
+
+            // HTTP client
+            HttpClient httpClient = new();
+
+            bool hasData;
+            do
+            {
+                var url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={page}";
+
+                // GET request
+                using var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = httpClient.Send(request);
+                response.EnsureSuccessStatusCode();
+
+                using var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+                // Deserialize and return response
+                var root = JsonConvert.DeserializeObject<Root>(streamReader.ReadToEnd());
+
+                foreach (var game in root.data)
+                {
+                    lGames.Add(game);
+                }
+
+                hasData = root.data != null && root.data.Count > 0;
+                page++;
+
+            } while (hasData);
+
+
+            page = 1;
+            do
+            {
+                var url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team2={team}&page={page}";
+
+                // GET request
+                using var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = httpClient.Send(request);
+                response.EnsureSuccessStatusCode();
+
+                using var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+                // Deserialize and return response
+                var root = JsonConvert.DeserializeObject<Root>(streamReader.ReadToEnd());
+
+                foreach (var game in root.data)
+                {
+                    lGames.Add(game);
+                }
+
+                hasData = root.data != null && root.data.Count > 0;
+                page++;
+
+            } while (hasData);
+
+
+            httpClient.Dispose();
+
+            return lGames;
+
+        }
+        public static string GetWinner(string competition, int year)
+        {
+            // HTTP client
+            HttpClient httpClient = new();
+
+            var url = $"https://jsonmock.hackerrank.com/api/football_competitions?name={competition}&year={year}";
+
+            // GET request
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = httpClient.Send(request);
+            response.EnsureSuccessStatusCode();
+
+            using var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+            // Deserialize and return response
+            var root = JsonConvert.DeserializeObject<Root2>(streamReader.ReadToEnd());
+
+
+            httpClient.Dispose();
+
+            return root.data[0].winner;
+
+        }
+
         public static int activityNotifications(List<int> expenditure, int d)
         {
             if (expenditure.Count <= d)
