362ec6ea2ae74e80da9367499073f49100d0b21c
Felipe Daiello <fdaiello@misterpostman.com.br>
2023-07-16 09:13:48 -0300

    checkMagazine - Hash problem; WhatFlavor

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 
 namespace InterviewPrep
 {
@@ -7,7 +8,42 @@ namespace InterviewPrep
     {
         static void Main(string[] args)
         {
-            TestCheckMagazine();
+            TestWhatFlavors();
+        }
+        public static void WhatFlavors(List<int> cost, int money)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < cost.Count-1; i++) 
+            {
+                if (map.ContainsKey(money - cost[i]))
+                {
+                    int i2 = map[money - cost[i]];
+                    Console.WriteLine(i2>i ? $"{i+1} {i2+1}" : $"{i2+1} {i+1}");
+                    return;
+                }
+                else
+                {
+                    map.Add(cost[i], i);
+                }
+            }
+        }
+        public static void WhatFlavors0(List<int> cost, int money)
+        {
+            for (int i = 0; i < cost.Count - 1; i++)
+            {
+                int i2 = cost.IndexOf(money - cost[i]);
+
+                if (i2 >= 0 && i2 != i)
+                {
+                    Console.WriteLine(i2 > i ? $"{i + 1} {i2 + 1}" : $"{i2 + 1} {i + 1}");
+                    return;
+                }
+            }
+        }
+        public static void TestWhatFlavors()
+        {
+            WhatFlavors(new List<int>() { 2, 2, 4, 3 }, 4);
         }
         public static void checkMagazine(List<string> magazine, List<string> note)
         {
